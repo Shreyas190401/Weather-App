7fa500a076bbc0b896ad82cac5cd20e40ea7cd62
Shreyas190401 <shreyasnigudkar16@gmail.com>
2023-11-26 14:08:29 +0530

    added function to get data from api

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const apiKey = "a9079cf632cac34da9a1f341d05bcd80";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=mumbai";
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+}
+
+
+checkWeather();
